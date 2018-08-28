9ad1e00aac9733dfe80d765458b73a8ba9d96523
faja <mcabaj@gmail.com>
2018-08-28 10:54:31 +0200

    v1.0.1

--- dep2.go
+++ dep2.go
@@ -1,5 +1,11 @@
 package dep2
 
+import dep1 "github.com/faja/gomodules-dep1"
+
 func Func1() string {
 	return "[dep2][v1.0.0] Func1"
+}
+
+func Func2_from_dep1() string {
+	return dep1.Func1()
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/faja/gomodules-dep2
+
+require github.com/faja/gomodules-dep1 v1.0.4
